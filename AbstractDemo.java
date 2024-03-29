19ad911741f66ab40de3acc93f4740e96c02c513
Monali Ranjun <165270530+code-with-monali@users.noreply.github.com>
2024-03-29 04:30:37 -0700

    Add files via upload

---
+++ AbstractDemo.java
@@ -0,0 +1,22 @@
+abstract RBI class
+{
+   public abstract int calculateROI();
+   public void DisplayRules()
+   System.out.println("You have to submmit addhar card and pancard");
+
+
+}
+
+Derived class extends RBI
+{
+
+}
+
+
+class AbstractDemo
+{
+    public static void main(String Arg[])
+{
+
+}
+}
